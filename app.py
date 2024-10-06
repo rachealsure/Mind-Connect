1f10c6518418a7f2fe6a5c9d9f6ed2f7d2f1406c
racheal-sure <mnyaboe33@gmail.com>
2024-10-06 09:57:15 +0300

    update

--- app.py
+++ app.py
@@ -150,6 +150,7 @@ def book_appointment():
 
 # Start the Flask app
 if __name__ == '__main__':
-    db.create_all()
-    app.run(port=5500, debug=True)
+      with app.app_context():
+        db.create_all()
+app.run(port=5500, debug=True)
     
